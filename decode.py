3844cbb9b51b72349536d32bd43fb669ba72fa16
Adrian Sampson <adrian@radbox.org>
2011-11-08 16:30:55 -0800

    decode.py now uses the wave module to ouput .wav files

--- decode.py
+++ decode.py
@@ -1,16 +1,23 @@
-"""Command-line tool to decode audio files to raw PCM."""
+"""Command-line tool to decode audio files to WAV files."""
 import audioread
 import sys
 import os
+import wave
+import contextlib
 
 def decode(filename):
     filename = os.path.abspath(os.path.expanduser(filename))
     with audioread.audio_open(filename) as f:
         print 'Input file: %i channels at %i Hz; %.1f seconds.' % \
               (f.channels, f.samplerate, f.duration)
-        with open(filename + '.pcm', 'wb') as of:
+
+        with contextlib.closing(wave.open(filename + '.wav', 'w')) as of:
+            of.setnchannels(f.channels)
+            of.setframerate(f.samplerate)
+            of.setsampwidth(2)
+
             for buf in f:
-                of.write(buf)
+                of.writeframes(buf)
 
 if __name__ == '__main__':
     decode(sys.argv[1])
