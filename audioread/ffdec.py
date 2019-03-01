0e3a4ce256345a50e97bba791159a84b2bfa9ee1
Florian Kaldowski <flokX@users.noreply.github.com>
2019-03-01 10:19:11 +0100

    Here are your trailing commas

--- audioread/ffdec.py
+++ audioread/ffdec.py
@@ -105,7 +105,7 @@ def available():
         COMMANDS,
         ['-version'],
         stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
+        stderr=subprocess.PIPE,
     )
     proc.wait()
     return (proc.returncode == 0)
@@ -142,7 +142,7 @@ class FFmpegAudioFile(object):
                 ['-i', filename, '-f', 's16le', '-'],
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
-                stdin=self.devnull
+                stdin=self.devnull,
             )
 
         except OSError:
